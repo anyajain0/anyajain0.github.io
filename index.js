5887bc497f0f7c4ca371638ea9c7e987942b353e
anyajain0 <anyajain110@gmail.com>
2025-06-16 23:46:20 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "3vw";
+document.getElementsByTagName("h2")[0].style.fontSize = "6vw";
+document.getElementsByTagName("h3")[0].style.fontSize = "3vw";
+document.getElementsByTagName("h4")[0].style.fontSize = "6vw";
